2d133224dfa6ba8d62230773a47f760bef5edd3e
Efren <50155831+EfrenAS@users.noreply.github.com>
2023-09-23 19:12:47 -0500

    Clean to images file that no use into project

--- views/portfolio/index.php
+++ views/portfolio/index.php
@@ -1,137 +1,247 @@
 <header class="header">
-  <div class="logo">Efren Anastacio Simeon</div>
-  <nav>
-    <a href="#">Hola, soy Efren</a>
-    <a href="#">Proyectos</a>
-    <a href="#">Tecnologias</a>
-    <a href="#">Idiomas</a>
-    <a href="#">Formacion</a>
-    <a href="#">Contacto</a>
-    <a href="#">Descargar CV</a>
-  </nav>
+  <div class="top-menu">
+    <div class="logo">Efren Anastacio Simeon</div>
+    <nav class="menu">
+      <a href="#">Hola, soy Efren</a>
+      <a href="#">Proyectos</a>
+      <a href="#">Conocimientos</a>
+      <a href="#">Idiomas</a>
+      <a href="#">Formacion</a>
+      <a href="#">Contacto</a>
+    </nav>
+  </div>
+  </div>
+  <div class="about-me">
+    <div class="container-title">
+      <h1>Desarrollador <span class="text-shadow">FullStack</span></h1>
+      <div class="container-social-networks">
+        <a href="#">
+          <picture>
+            <!-- <source srcset="build/img/social_media/github.avif" type="image/avif">
+            <source srcset="build/img/social_media/github.webp" type="image/webp"> -->
+            <img loading="lazy" src="build/img/social_media/github.svg" alt="Logo GitHub">
+          </picture>
+        </a>
+        <a href="#" class="link-social">
+          <picture>
+            <!-- <source srcset="build/img/social_media/linkedin.avif" type="image/avif">
+            <source srcset="build/img/social_media/linkedin.webp" type="image/webp"> -->
+            <img loading="lazy" src="build/img/social_media/linkedin.svg" alt="Logo LinkedIn">
+          </picture>
+        </a>
+        <a href="#" class="link-social">
+          <picture>
+            <!-- <source srcset="build/img/social_media/telegram.avif" type="image/avif">
+            <source srcset="build/img/social_media/telegram.webp" type="image/webp"> -->
+            <img loading="lazy" width="100px" height="100px" src="build/img/social_media/facebook.svg" alt="Logo Telegram">
+          </picture>
+        </a>
+      </div>
+    </div>
+    <div class="container-resume">
+      <p class="profile-name"><span class="subtitle">Hola soy</span>Efren Anastacio Simeon.</p>
+      <p class="about-me">Ingeniero en Sistemas Computacionales con especialidad en Sistemas Distribuidos y 3 anos de experiencia en el desarrollo web, veo la oportunidad de aplicar la tecnologia en cada problema que se me presenta, suelo aportar y recibir ideas para llegar a la meta lo mas rapido posible.</p>
+      <a href="#" class="link-download"><span>&darr;</span> Descargar CV</a>
+    </div>
+  </div>
 </header>
-<main>
-  <h1>Desarrollador Web / Freelance</h1>
-  <p>
-    “La tecnología no es nada. Lo importante es que tengas fe en la gente, que sean básicamente buenas e inteligentes, y si les das herramientas, harán cosas maravillosas con ellas”. Steve Jobs, cofundador y presidente ejecutivo de Apple Inc.
-  </p>
-  <section>
-    <h2>Ingeniero en Sistemas Computacionales</h2>
-    <p>Con 3 anos de experiencia en el desarrollo web, veo la oportunidad de aplicar la tecnologia en cada problema que se me presenta, suelo aportar y recibir ideas para llegar a la meta lo mas rapido posible.</p>
-  </section>
+<main class="container">
   <section>
     <h2>Proyectos</h2>
   </section><!-- Proyectos -->
-  <section>
-    <h2>Tecnologias</h2>
-    <div>
-      <h3>HTML 5</h3>
-      <div>
-        <picture>
-          <source srcset="build/img/logos-dev/html5.avif" type="image/avif">
-          <source srcset="build/img/logos-dev/html5.webp" type="image/webp">
-          <img loading="lazy" src="build/img/logos-dev/html5.png" alt="Logo HTML5">
-        </picture>
+  <section class="skills">
+    <h2>Conocimientos</h2>
+    <div class="skills__container">
+      <div class="card">
+        <div class="card-header">
+          <h3>HTML 5</h3>
+        </div>
+        <div class="card-body">
+          <div class="card-image">
+            <picture>
+              <source srcset="build/img/logos-dev/html5.avif" type="image/avif">
+              <source srcset="build/img/logos-dev/html5.webp" type="image/webp">
+              <img loading="lazy" src="build/img/logos-dev/html5.png" alt="Logo HTML5">
+            </picture>
+          </div>
+          <div class="card-text">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            <p>Expedita iure eos sequi cupiditate non laudantium minima reprehenderit incidunt.</p>
+          </div>
+        </div>
+      </div><!-- .card -->
+      <div class="card">
+        <div class="card-header">
+          <h3>CSS 3</h3>
+        </div>
+        <div class="card-body">
+          <div class="card-image">
+            <picture>
+              <source srcset="build/img/logos-dev/css3.avif" type="image/avif">
+              <source srcset="build/img/logos-dev/css3.webp" type="image/webp">
+              <img loading="lazy" src="build/img/logos-dev/css3.png" alt="Logo CSS3">
+            </picture>
+          </div>
+          <div class="card-text">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            <p>Expedita iure eos sequi cupiditate non laudantium minima reprehenderit incidunt.</p>
+          </div>
+        </div>
+      </div><!-- .card -->
+      <div class="card">
+        <div class="card-title">
+          <h3>Javascript</h3>
+        </div>
+        <div class="card-body">
+          <div class="card-image">
+            <picture>
+              <source srcset="build/img/logos-dev/javascript.avif" type="image/avif">
+              <source srcset="build/img/logos-dev/javascript.webp" type="image/webp">
+              <img loading="lazy" src="build/img/logos-dev/javascript.png" alt="Logo Javascript">
+            </picture>
+          </div>
+          <div class="card-text">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            <p>Expedita iure eos sequi cupiditate non laudantium minima reprehenderit incidunt.</p>
+          </div>
+        </div>
+      </div><!-- .card -->
+      <div class="card">
+        <div class="card-title">
+          <h3>SASS</h3>
+        </div>
+        <div class="card-body">
+          <div class="card-image">
+            <picture>
+              <source srcset="build/img/logos-dev/sass.avif" type="image/avif">
+              <source srcset="build/img/logos-dev/sass.webp" type="image/webp">
+              <img loading="lazy" src="build/img/logos-dev/sass.png" alt="Logo SASS">
+            </picture>
+          </div>
+          <div class="card-text">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            <p>Expedita iure eos sequi cupiditate non laudantium minima reprehenderit incidunt.</p>
+          </div>
+        </div><!-- .card -->
       </div>
-      <div>
-        <p>Skills</p>
+      <div class="card">
+        <div class="card-title">
+          <h3>GULP</h3>
+        </div>
+        <div class="card-body">
+          <div class="card-image">
+            <picture>
+              <source srcset="build/img/logos-dev/gulp.avif" type="image/avif">
+              <source srcset="build/img/logos-dev/gulp.webp" type="image/webp">
+              <img loading="lazy" src="build/img/logos-dev/gulp.png" alt="Logo gulp">
+            </picture>
+          </div>
+          <div class="card-text">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            <p>Expedita iure eos sequi cupiditate non laudantium minima reprehenderit incidunt.</p>
+          </div>
+        </div> <!-- .card -->
       </div>
-    </div>
-    <div>
-      <h3>CSS 3</h3>
-      <div>
-        <picture>
-          <source srcset="build/img/logos-dev/css3.avif" type="image/avif">
-          <source srcset="build/img/logos-dev/css3.webp" type="image/webp">
-          <img loading="lazy" src="build/img/logos-dev/css3.png" alt="Logo CSS3">
-        </picture>
-      </div>
-      <div>
-        <p>Skills</p>
-      </div>
-    </div>
-    <div>
-      <h3>Javascript</h3>
-      <div>
-        <picture>
-          <source srcset="build/img/logos-dev/javascript.avif" type="image/avif"> 
-          <source srcset="build/img/logos-dev/javascript.webp" type="image/webp">
-          <img loading="lazy" src="build/img/logos-dev/javascript.png" alt="Logo Javascript">
-        </picture>
-      </div>
-      <div>
-        <p>Skills</p>
-      </div>
-    </div>
-    <picture>
-    <div>
-      <h3>SASS</h3>
-      <div>
-        <picture>
-          <source srcset="build/img/logos-dev/sass.avif" type="image/avif">
-          <source srcset="build/img/logos-dev/sass.webp" type="image/webp">
-          <img loading="lazy" src="build/img/logos-dev/sass.png" alt="Logo SASS">
-        </picture>
-      </div>
-      <div>
-        <p>Skills</p>
-      </div>
-    </div>
-    <div>
-      <h3>GULP</h3>
-      <div>
-        <picture>
-          <source srcset="build/img/logos-dev/gulp.avif" type="image/avif">
-          <source srcset="build/img/logos-dev/gulp.webp" type="image/webp">
-          <img loading="lazy" src="build/img/logos-dev/gulp.png" alt="Logo gulp">
-        </picture>
-      </div>
-      <div>
-        <p>Skills</p>
-      </div>
-    </div>
-    <div>
-      <h3>PHP</h3>
-      <div>
-        <picture>
-          <source srcset="build/img/logos-dev/php.avif" type="image/avif">
-          <source srcset="build/img/logos-dev/php.webp" type="image/webp">
-          <img loading="lazy" src="build/img/logos-dev/php.png" alt="Logo PHP">
-        </picture>
-      </div>
-      <div>
-        <p>Skills</p>
-      </div>
-    </div>
-    <div>
-      <h3>MySQL</h3>
-      <div>
-        <picture>
-          <source srcset="build/img/logos-dev/MySQL.avif" type="image/avif">
-          <source srcset="build/img/logos-dev/MySQL.webp" type="image/webp">
-          <img loading="lazy" src="build/img/logos-dev/MySQL.png" alt="Logo MySQL">
-        </picture>
-      </div>
-      <div>
-        <p>Skills</p>
-      </div>
+      <div class="card">
+        <div class="card-title">
+          <h3>PHP</h3>
+        </div>
+        <div class="card-body">
+          <div class="card-image">
+            <picture>
+              <source srcset="build/img/logos-dev/php.avif" type="image/avif">
+              <source srcset="build/img/logos-dev/php.webp" type="image/webp">
+              <img loading="lazy" src="build/img/logos-dev/php.png" alt="Logo PHP">
+            </picture>
+          </div>
+          <div class="card-text">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            <p>Expedita iure eos sequi cupiditate non laudantium minima reprehenderit incidunt.</p>
+          </div>
+        </div>
+      </div> <!-- .card -->
+      <div class="card">
+        <div class="card-title">
+          <h3>MySQL</h3>
+        </div>
+        <div class="card-body">
+          <div class="card-image">
+            <picture>
+              <source srcset="build/img/logos-dev/MySQL.avif" type="image/avif">
+              <source srcset="build/img/logos-dev/MySQL.webp" type="image/webp">
+              <img loading="lazy" src="build/img/logos-dev/MySQL.png" alt="Logo MySQL">
+            </picture>
+          </div>
+          <div class="card-text">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            <p>Expedita iure eos sequi cupiditate non laudantium minima reprehenderit incidunt.</p>
+          </div>
+        </div>
+      </div> <!-- .card -->
     </div>
   </section><!-- Conocimientos -->
   <section>
     <h2>Idiomas</h2>
+    <p>Espanol</p>
+    <p>Ingles</p>
   </section><!-- Idiomas -->
   <section>
     <h2>Formacion</h2>
   </section><!-- Formacion -->
-  <section>
-    <h2>Contacto</h2>
-  </section><!-- Contacto -->
 </main>
+<section>
+  <h2>Contacto</h2>
+  <div>
+    <picture>
+      <source srcset="build/img/social_media/github.avif" type="image/avif">
+      <source srcset="build/img/social_media/github.webp" type="image/webp">
+      <img loading="lazy" src="build/img/social_media/github.png" alt="Logo GitHub">
+    </picture>
+    <picture>
+      <source srcset="build/img/social_media/linkedin.avif" type="image/avif">
+      <source srcset="build/img/social_media/linkedin.webp" type="image/webp">
+      <img loading="lazy" src="build/img/social_media/linkedin.png" alt="Logo LinkedIn">
+    </picture>
+    <picture>
+      <source srcset="build/img/social_media/telegram.avif" type="image/avif">
+      <source srcset="build/img/social_media/telegram.webp" type="image/webp">
+      <img loading="lazy" src="build/img/lsocial_media/telegram.png" alt="Logo Telegram">
+    </picture>
+    <picture>
+      <source srcset="build/img/social_media/whatsapp.avif" type="image/avif">
+      <source srcset="build/img/social_media/whatsapp.webp" type="image/webp">
+      <img loading="lazy" src="build/img/social_media/whatsapp.png" alt="Logo WhatsApp">
+    </picture>
+  </div>
+  <form action="">
+    <caption>O si lo prefieres, dime en que colaboramos juntos y me pongo en contacto contigo</caption>
+    <div>
+      <label for="name">Nombre(s)</label>
+      <input name="name" type="text" placeholder="nombre">
+    </div>
+    <div>
+      <label for="name">Apellido</label>
+      <input name="name" type="text" placeholder="Apellidos">
+    </div>
+    <div>
+      <label for="phone">Telefono</label>
+      <input name="phone" type="tel" placeholder="442-1234567">
+    </div>
+    <div>
+      <label for="email">Correo Electronico</label>
+      <input name="email" type="email" placeholder="micorreo@micorreo.com">
+    </div>
+    <div>
+      <label for="comments">Explicame un poco</label>
+      <textarea name="comments" id="comments" cols="30" rows="10"></textarea>
+    </div>
+  </form>
+</section><!-- Contacto -->
 <footer>
   <nav>
     <a href="#">Hola, soy Efren</a>
     <a href="#">Proyectos</a>
-    <a href="#">Tecnologias</a>
+    <a href="#">Conocimientos</a>
     <a href="#">Idiomas</a>
     <a href="#">Formacion</a>
     <a href="#">Contacto</a>
